09bb4288204f8f5b8882208244f01dab571ea268
Anna Balabushko <anna.balabushko92@gmail.com>
2023-12-25 11:58:32 +0100

    add autentication step

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -2,8 +2,6 @@
 namespace Tests;
 
 use Tests\Support\ApiTester;
-use Codeception\Attribute\Incomplete;
-use DateTime;
 
 class ApiCest 
 {    
@@ -11,6 +9,8 @@ class ApiCest
 
     public function createUser(ApiTester $I)
     {
+        $bearerToken = $I->getConfig('api')['bearerToken'];
+        $I->amBearerAuthenticated($bearerToken);
         $I->sendPost(self::USERS_URL, ['name' => 'username', 'gender' => 'male', 'email' => random_int(0, 100) . '@test.com', 'status' => 'active']);
 
         $I->seeResponseCodeIs(201);
