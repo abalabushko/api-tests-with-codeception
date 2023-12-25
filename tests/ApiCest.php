bf8673d49c9aa72c4386f438ef8c612584e8c67e
Anna Balabushko <anna.balabushko92@gmail.com>
2023-12-25 11:44:20 +0100

    add test create a new user

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -3,14 +3,18 @@ namespace Tests;
 
 use Tests\Support\ApiTester;
 use Codeception\Attribute\Incomplete;
+use DateTime;
 
 class ApiCest 
 {    
-    #[Incomplete]
-    public function tryApi(ApiTester $I)
+    public const USERS_URL = '/users/';
+
+    public function createUser(ApiTester $I)
     {
-        $I->sendGet('/');
-        $I->seeResponseCodeIs(200);
+        $I->sendPost(self::USERS_URL, ['name' => 'username', 'gender' => 'male', 'email' => random_int(0, 100) . '@test.com', 'status' => 'active']);
+
+        $I->seeResponseCodeIs(201);
         $I->seeResponseIsJson();
+        $I->seeResponseContains('username');
     }
 }
