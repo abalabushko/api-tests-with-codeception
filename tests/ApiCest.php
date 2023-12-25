494d3629194410cb6104ddd7a428350148efad47
Anna Balabushko <anna.balabushko92@gmail.com>
2023-12-25 12:20:26 +0100

    empty line to added, array fixes

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -7,11 +7,18 @@ class ApiCest
 {    
     public const USERS_URL = '/users/';
 
-    public function createUser(ApiTester $I)
+    public function createUser(ApiTester $I): void
     {
         $bearerToken = getenv('API_BEARER_TOKEN');
         $I->amBearerAuthenticated($bearerToken);
-        $I->sendPost(self::USERS_URL, ['name' => 'username', 'gender' => 'male', 'email' => random_int(0, 100) . '@test.com', 'status' => 'active']);
+        $I->sendPost(self::USERS_URL,
+        [
+            'name' => 'username',
+            'gender' => 'male',
+            'email' => random_int(0, 100) . '@test.com',
+            'status' => 'active'
+            ]
+        );
 
         $I->seeResponseCodeIs(201);
         $I->seeResponseIsJson();
